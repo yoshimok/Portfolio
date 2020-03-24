7121c6e26ffd82b1baee85aa41338d852f7bcbda
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-03-24 12:22:29 +0900

    initialize by create-react-app and change directory

---
+++ src/pages/mainPage/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import logo from "../../assets/icons/logo.svg";
+import "./App.css";
+
+function App() {
+  const [speed, setSpeed] = useState(20);
+
+  const spinProperty = {
+    animation: `App-logo-spin infinite ${speed}s linear`
+  };
+
+  const up = () => {
+    if (speed < 2) {
+      setSpeed(speed - 0.05);
+
+      return;
+    }
+    setSpeed(speed - 2);
+  };
+
+  const down = () => {
+    setSpeed(speed + 2);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" style={spinProperty} />
+
+        <div className={"button-group"}>
+          <div className="button" onClick={up}>
+            ↑
+          </div>
+          <div className="button" onClick={down}>
+            ↓
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
