e702e962fe11816e63bc0f6395444f4f8ce49e2c
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-07-25 16:58:35 +0900

    add: added models directory and change interfaces. it will clean

---
+++ src/model/interfaces.ts
@@ -0,0 +1,12 @@
+export interface IProfile {
+  name: string;
+  icon: string;
+  position: string;
+}
+
+export interface IUrlInfo {
+  accountName: string;
+  serviceName: string;
+  serviceIcon: string;
+  url: string;
+}
