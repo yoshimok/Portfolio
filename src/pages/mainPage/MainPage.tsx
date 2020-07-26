f9d019976e136d414c57aa48a6a63377c53a255b
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-07-26 23:29:01 +0900

    change: change file name App -> mainpage

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import logo from "assets/icons/logo.svg";
 import "./App.scss";
+import styles from "./MainPage.module.scss";
 
 import { AuthorProfile } from "./sections/AuthorProfile/AuthorProfile";
 import { Biography } from "./sections/Biography/Biography";
+import { Header } from "components/organisms/Header/Header";
 
 import githubLogo from "assets/icons/gitHub.svg";
 import twitterLogo from "assets/icons/twitter.svg";
@@ -23,13 +25,13 @@ const mockProfile = {
         accountName: "yoshimok",
         serviceName: "GitHub",
         serviceIcon: githubLogo,
-        url: "https://github.com/yoshimok/Portfolio",
+        url: "https://github.com/yoshimok",
       },
       {
         accountName: "@yoshimok__",
         serviceName: "Twitter",
         serviceIcon: twitterLogo,
-        url: "https://github.com/yoshimok/Portfolio",
+        url: "https://twitter.com/yoshimok__",
       },
       {
         accountName: "@yoshimok",
@@ -41,6 +43,25 @@ const mockProfile = {
   },
   biography:
     "自由気ままにITベンチャーでエンジニアしています。興味のある分野はたくさん。得意分野はアプリケーション開発です。よろしくお願いします。",
+};
+
+const mockHeaderProps = {
+  headerTitle: "HeaderTitle",
+  clickHamburger: () => {},
+  contents: [
+    {
+      title: "contents1",
+      href: "http://localhost:3001#profile",
+    },
+    {
+      title: "contents2",
+      href: "http://localhost:3001#guruguru",
+    },
+    {
+      title: "contents3",
+      href: "http://localhost9009",
+    },
+  ],
 };
 
 export const MainPage: React.FC<Props> = (props: Props) => {
@@ -62,14 +83,20 @@ export const MainPage: React.FC<Props> = (props: Props) => {
   const down = () => {
     setSpeed(speed + 2);
   };
+
   return (
     <div>
-      <div id="profile">
-        <AuthorProfile {...mockProfile} />
-        <Biography bioText={mockProfile.biography} />
+      <Header {...mockHeaderProps} />
+      <div className={styles.body}>
+        <div className={styles.profile}>
+          <AuthorProfile {...mockProfile} />
+        </div>
+        <div className={styles.contents}>
+          <Biography bioText={mockProfile.biography} />
+        </div>
       </div>
       <div className="App">
-        <header className="App-header">
+        <header id="guruguru" className="App-header">
           <img
             src={logo}
             className="App-logo"
