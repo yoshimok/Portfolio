0e6ad4591134371193c61d7cc524064a45d52a83
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-07-26 22:34:28 +0900

    add: add profilesection in main page

--- src/pages/mainPage/App.tsx
+++ src/pages/mainPage/App.tsx
@@ -2,7 +2,48 @@ import React, { useState } from "react";
 import logo from "assets/icons/logo.svg";
 import "./App.scss";
 
-function App() {
+import { AuthorProfile } from "./sections/AuthorProfile/AuthorProfile";
+import { Biography } from "./sections/Biography/Biography";
+
+import githubLogo from "assets/icons/gitHub.svg";
+import twitterLogo from "assets/icons/twitter.svg";
+import codepenLogo from "assets/icons/codepen.svg";
+
+export type Props = {};
+
+const mockProfile = {
+  authorInfo: {
+    profile: {
+      name: "Tom",
+      icon: "https://i.pravatar.cc/300",
+      position: "Software Enfineer",
+    },
+    services: [
+      {
+        accountName: "yoshimok",
+        serviceName: "GitHub",
+        serviceIcon: githubLogo,
+        url: "https://github.com/yoshimok/Portfolio",
+      },
+      {
+        accountName: "@yoshimok__",
+        serviceName: "Twitter",
+        serviceIcon: twitterLogo,
+        url: "https://github.com/yoshimok/Portfolio",
+      },
+      {
+        accountName: "@yoshimok",
+        serviceName: "Codepen",
+        serviceIcon: codepenLogo,
+        url: "https://github.com/yoshimok/Portfolio",
+      },
+    ],
+  },
+  biography:
+    "自由気ままにITベンチャーでエンジニアしています。興味のある分野はたくさん。得意分野はアプリケーション開発です。よろしくお願いします。",
+};
+
+export const MainPage: React.FC<Props> = (props: Props) => {
   const [speed, setSpeed] = useState(20);
 
   const spinProperty = {
@@ -22,20 +63,29 @@ function App() {
     setSpeed(speed + 2);
   };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" style={spinProperty} />
-        <div className={"button-group"}>
-          <div className="button" onClick={up}>
-            ↑
+    <div>
+      <div id="profile">
+        <AuthorProfile {...mockProfile} />
+        <Biography bioText={mockProfile.biography} />
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={spinProperty}
+          />
+          <div className={"button-group"}>
+            <div className="button" onClick={up}>
+              ↑
+            </div>
+            <div className="button" onClick={down}>
+              ↓
+            </div>
           </div>
-          <div className="button" onClick={down}>
-            ↓
-          </div>
-        </div>
-      </header>
+        </header>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import App from "./pages/mainPage/App";
+import { MainPage } from "./pages/mainPage/App";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" component={MainPage} />
       </Switch>
     </BrowserRouter>
   );
