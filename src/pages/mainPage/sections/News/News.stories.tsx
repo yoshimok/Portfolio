6a364758a0a180e7bbbfd9adcb6ef907220e4f30
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-07-27 11:21:35 +0900

    add: add section Newa

---
+++ src/pages/mainPage/sections/News/News.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { News, Props } from "./News";
+
+export default {
+  title: "Sections/News",
+  component: News,
+};
+
+const mockProps: Props = {
+  headline: "News Title",
+  contents: [
+    {
+      title: "News Title",
+      body:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis vulputate dui. Nulla vestibulum sapien ac massa pharetra rutrum. Pellentesque lacus sapien, efficitur in mattis at, hendrerit quis tortor. Cras ac auctor lacus. Aliquam non justo id purus dignissim mattis sed viverra metus. Phasellus quis facilisis arcu. Donec molestie tortor eu vestibulum porta. Aliquam feugiat felis turpis, facilisis porta turpis pharetra ut. Vivamus ante nunc, ullamcorper at ante porttitor, luctus commodo massa.",
+      datetime: "2020-07-19T10:39:35.725Z",
+    },
+    {
+      title: "News Title",
+      body:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis vulputate dui. Nulla vestibulum sapien ac massa pharetra rutrum. Pellentesque lacus sapien, efficitur in mattis at, hendrerit quis tortor. Cras ac auctor lacus. Aliquam non justo id purus dignissim mattis sed viverra metus. Phasellus quis facilisis arcu. Donec molestie tortor eu vestibulum porta. Aliquam feugiat felis turpis, facilisis porta turpis pharetra ut. Vivamus ante nunc, ullamcorper at ante porttitor, luctus commodo massa.",
+      datetime: "2020-07-19T10:39:35.725Z",
+    },
+  ],
+};
+
+export const Default = () => <News {...mockProps} />;
