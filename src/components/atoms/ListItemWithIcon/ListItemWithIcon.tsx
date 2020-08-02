2d3cfd0b7a69f6cb1129a6a12398a249d263c65b
yoshimok <yoshiko_yoshiyoshi@icloud.com>
2020-08-02 12:43:56 +0900

    change: change: layout and add responsive comopnent list with icon

--- src/components/atoms/ListItemWithIcon/ListItemWithIcon.tsx
+++ src/components/atoms/ListItemWithIcon/ListItemWithIcon.tsx
@@ -4,12 +4,17 @@ import styles from "./ListItemWithIcon.module.scss";
 export type Props = {
   icon: string;
   title: string;
-  click: () => void;
+  click?: () => void;
+  center?: boolean;
 };
 
 export const ListItemWithIcon: React.FC<Props> = (props: Props) => {
+  const listStyle = {
+    cursor: props.click ? "pointer" : undefined,
+    justifyContent: props.center ? "center" : "start",
+  };
   return (
-    <div className={styles.container} onClick={props.click}>
+    <div className={styles.container} onClick={props.click} style={listStyle}>
       <img className={styles.itemIcon} src={props.icon} alt="itemiIcon" />
       <span className={styles.title}>{props.title}</span>
     </div>
